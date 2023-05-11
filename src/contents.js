1c34299e9e63a8fcf0af97debd4fa2076a0a554c
Claudia Tohalino <claudia.tohalino@gmail.com>
2023-05-11 14:37:50 -0500

    primeros pasos

---
+++ src/contents.js
@@ -0,0 +1,4 @@
+/* eslint-disable semi */
+export const logoKetoLife = `<div id="logoketolife" class="logoketolife">
+<img src="imagenes/LogoKetoLife.png">
+</div>`
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 // Este es el punto de entrada de tu aplicacion
+import { logoKetoLife } from './contents.js';
 
-import { myFunction } from './lib/index.js';
+const contenedorUniversal = document.getElementById('contenedorUniversal');
+const contenedorLogoKetoLife = document.createElement('div');
+contenedorLogoKetoLife.innerHTML = logoKetoLife;
+contenedorUniversal.appendChild(contenedorLogoKetoLife);
 
-myFunction();
+// import { myFunction } from './lib/index.js';
+// *myFunction();
