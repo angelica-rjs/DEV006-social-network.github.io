ffd83c3f9e8c0862194045cf2186408bea1d5107
Claudia Tohalino <claudia.tohalino@gmail.com>
2023-05-11 17:09:04 -0500

    funcion v1 sin botones

--- src/main.js
+++ src/main.js
@@ -13,6 +13,21 @@ function mostrarLogo() {
 // Vista1-frame16
 function vista1() {
   mostrarLogo();
+  // imagen del sarten de bienvenida
+  const imagenBienvenida = document.createElement('div');
+  imagenBienvenida.innerHTML = '<img src="imagenes/ImagenBienvenida.png">';
+  imagenBienvenida.classList = 'imagenBienvenida';
+  contenedorUniversal.appendChild(imagenBienvenida);
+  // parrafos de bienvenida
+  const h1Bienvenida = document.createElement('div');
+  h1Bienvenida.innerHTML = '<h1> ¡Bienvenido! </h1>';
+  h1Bienvenida.classList = 'h1Bienvenida';
+  contenedorUniversal.appendChild(h1Bienvenida);
+  // parrafo que viene despues del bienvenida
+  const parrafoBienvenida = document.createElement('div');
+  parrafoBienvenida.innerHTML = '<p> Mas que una dieta un estilo de vida </p>';
+  parrafoBienvenida.classList = 'parrafoBienvenida';
+  contenedorUniversal.appendChild(parrafoBienvenida);
 }
 vista1();
 // contenedorUniversal.innerHTML = `${mostrarLogo}`;
